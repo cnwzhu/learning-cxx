5f4ecad0ed639fcc4d9b2a0ce0ffd8794aec599f
aka.wz <Wang.Zhu@iboxplus.net>
2024-07-29 17:35:41 +0800

    fix

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -11,7 +11,7 @@ struct Tensor {
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
-        memcpy(shape, shape_, N * sizeof(unsigned int));
+        std::wmemcpy(shape, shape_, N * sizeof(unsigned int));
         for (unsigned int i = 0; i < N; ++i) {
             size *= shape[i];
         }
